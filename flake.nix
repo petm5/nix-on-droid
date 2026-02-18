054b3c3853429a7fa4765d79293f96c2b07921b0
Peter Marshall <petms@proton.me>
2026-02-18 00:47:53 -0500

    Update bootstrap nixpkgs version

--- flake.nix
+++ flake.nix
@@ -7,7 +7,7 @@
     # for bootstrap zip ball creation and proot-termux builds, we use a fixed version of nixpkgs to ease maintanence.
     # head of nixos-24.05 as of 2024-07-06
     # note: when updating nixpkgs-for-bootstrap, update store paths of proot-termux in modules/environment/login/default.nix
-    nixpkgs-for-bootstrap.url = "github:NixOS/nixpkgs/49ee0e94463abada1de470c9c07bfc12b36dcf40";
+    nixpkgs-for-bootstrap.url = "github:NixOS/nixpkgs/fa56d7d6de78f5a7f997b0ea2bc6efd5868ad9e8";
 
     home-manager = {
       url = "github:nix-community/home-manager";
