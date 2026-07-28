d28417c7b68ce04149162eb29dd33dc2b92ca803
Peter Marshall <pm@petermarshall.ca>
2026-07-28 12:16:44 -0400

    tests/emulator: generate url from package info

--- tests/emulator/default.nix
+++ tests/emulator/default.nix
@@ -8,20 +8,24 @@
 , fetchurl
 }:
 let
+  package = "com.termux.nix";
+  revision = "188035";
+
+  abiVersion = if stdenv.isx86_64 then "x86_64"
+    else if stdenv.isAarch64 then "arm64-v8a"
+    else throw "Unsupported architecture ${stdenv.hostPlatform.system}";
+
   apkFile = fetchurl {
-    url = "https://nix-on-droid.unboiled.info/com.termux.nix_188035-x86_64.apk";
+    url = "https://nix-on-droid.unboiled.info/${package}_${revision}-${abiVersion}.apk";
     hash = "sha256-Vy9O+dgHSzIn9O1DZgjrrBGEs9pI8D/zvm85OqiV/1E=";
   };
 
   emulatorScript = androidenv.emulateApp {
     name = "nix-on-droid-emulator";
     platformVersion = "29";
-    abiVersion = if stdenv.isx86_64 then "x86_64"
-      else if stdenv.isAarch64 then "arm64-v8a"
-      else throw "Unsupported architecture ${stdenv.hostPlatform.system}";
     systemImageType = "default";
+    inherit package abiVersion;
     app = apkFile;
-    package = "com.termux.nix";
     androidEmulatorFlags = "-no-window -no-audio -no-boot-anim -gpu swiftshader_indirect";
   };
 in
