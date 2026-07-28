83c36d3354069352f87c6ceba569504ce97251e4
Peter Marshall <pm@petermarshall.ca>
2026-07-28 18:18:34 -0400

    tests: fetch apk from fdroid

--- tests/emulator/default.nix
+++ tests/emulator/default.nix
@@ -9,20 +9,15 @@
 }:
 let
   package = "com.termux.nix";
-  revision = "188035";
+  revision = "188037";
 
   abiVersion = if stdenv.isx86_64 then "x86_64"
-    # else if stdenv.isAarch64 then "arm64-v8a"
+    else if stdenv.isAarch64 then "arm64-v8a"
     else throw "Unsupported architecture ${stdenv.hostPlatform.system}";
 
-  hashes = {
-    "x86_64" = "sha256-Vy9O+dgHSzIn9O1DZgjrrBGEs9pI8D/zvm85OqiV/1E=";
-    # "arm64-v8a" = "";
-  };
-
   apkFile = fetchurl {
-    url = "https://nix-on-droid.unboiled.info/${package}_${revision}-${abiVersion}.apk";
-    hash = hashes.${abiVersion};
+    url = "https://f-droid.org/repo/${package}_${revision}.apk";
+    hash = "sha256-D2uoLWkpbDPGbuXRCqGEY+SVPCpsP9EOfneMLNrgMwM=";
   };
 
   emulatorScript = androidenv.emulateApp {
